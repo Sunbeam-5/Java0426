90330f577befff8f92225e6a6f36c6b3c425f8c5
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2022-05-20 23:42:58 +0800

    日常

---
+++ 20220513/src/两数之和.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 两数之和 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String arr = sc.nextLine();
+        long target = sc.nextLong();
+        for (int i = 0; i < arr.length(); i++) {
+            for (int j = 0; j < arr.length(); j++) {
+                
+            }
+        }
+    }
+}
