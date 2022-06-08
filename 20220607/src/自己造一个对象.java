0a64586b2c20b6c36155fdfb1282f59317de3568
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2022-06-08 16:10:50 +0800

    日常练习

--- 20220607/src/自己造一个对象.java
+++ 20220607/src/自己造一个对象.java
@@ -1,42 +1,49 @@
-public class 自己造一个对象 {
-    class girlfriend{
-        public String name;
-        public String Englishname;
-        public int age;
-        public int height;
-        public int weight;
-        public String number;
-        public String address;
+class Girlfriend{
+    public String name;
+    public String Englishname;
+    public int age;
+    public int height;
+    public int weight;
+    public String number;
+    public String address;
 
-        public void responde1(){
-            System.out.println("早安");
-        }
-        public void responde2(){
-            System.out.println("午安");
-        }
-        public void responde3(){
-            System.out.println("晚安");
-        }
-        public void responde4(){
-            System.out.println("啊哈哈哈哈哈哈，憨憨");
-        }
-        public void responde5(){
-            System.out.println("小狐狸~");
-        }
-        public void responde6(){
-            System.out.println("憨批");
-        }
+    public void responde1(){
+        System.out.println(name+":"+"早安");
     }
+    public void responde2(){
+        System.out.println(name+":"+"午安");
+    }
+    public void responde3(){
+        System.out.println(name+":"+"晚安");
+    }
+    public void responde4(){
+        System.out.println(name+":"+"啊哈哈哈哈哈哈，憨憨");
+    }
+    public void responde5(){
+        System.out.println(name+":"+"小狐狸~");
+    }
+    public void responde6(){
+        System.out.println(name+":"+"憨批");
+    }
+}
+public class 自己造一个对象 {
     public static void main(String[] args) {
-        girlfriend the_little_prince = new girlfriend();
-        the_little_prince.name = "绾";
+        Girlfriend the_little_prince = new Girlfriend();
+        the_little_prince.name = "w";
         the_little_prince.Englishname = "Isbella";
         the_little_prince.age = 19;
         the_little_prince.height = 168;
         the_little_prince.weight = 103;
-        the_little_prince.number = "62";
-        the_little_prince.address = "CA";
+        the_little_prince.number = "626362xxxx";
+        the_little_prince.address = "Ontario, CA";
 
+        System.out.println(the_little_prince.name);
+        System.out.println(the_little_prince.Englishname);
+        System.out.println(the_little_prince.age);
+        System.out.println(the_little_prince.height);
+        System.out.println(the_little_prince.weight);
+        System.out.println(the_little_prince.number);
+        System.out.println(the_little_prince.address);
         the_little_prince.responde1();
     }
 }
